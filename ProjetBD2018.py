6f8d845cd2579311029f8a8ac8bfdd3aff66e8c8
SimLemay <simon.lemay-lauziere.1@ulaval.ca>
2018-03-07 21:11:37 -0500

    Some tests

--- ProjetBD2018.py
+++ ProjetBD2018.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.mysql import MySQL
 
 app = Flask(__name__)
 
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'username'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'ProjetBD'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/<name>')
+def hello(name=None):
+    cursor.execute('''SELECT * FROM test''')
+    data = cursor.fetchall()
+    return render_template('index.html', name=name, data=data)
 
 
 if __name__ == '__main__':
