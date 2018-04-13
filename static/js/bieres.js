d79a84832ce3b92e30552e650d1dabbe34add15f
SimLemay <simon.lemay-lauziere.1@ulaval.ca>
2018-04-13 16:23:40 -0400

    Ajout dans le panier

--- static/js/bieres.js
+++ static/js/bieres.js
@@ -2,3 +2,7 @@ $(document).ready(function () {
     $("#bouton-bieres").addClass("active");
     $("#selecteur-sous-sorte").select2({width: '350px'});
 });
+
+$(".ajout-biere-panier").submit(function () {
+    $(this).find(".redirect-url").val(window.location.href.replace(window.location.origin, ''));
+});
