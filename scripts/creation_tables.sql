d3934e31eb832e398ebc1226759e7b781754cf5a
SimLemay <simon.lemay-lauziere.1@ulaval.ca>
2018-04-13 13:49:46 -0400

    AUTO_INCREMENT pour les id

--- scripts/creation_tables.sql
+++ scripts/creation_tables.sql
@@ -1,69 +1,70 @@
 CREATE TABLE Sorte (
-    id INTEGER PRIMARY KEY,
-    aromes VARCHAR(500),
-    nom VARCHAR(100)
+  id     INTEGER PRIMARY KEY,
+  aromes VARCHAR(500),
+  nom    VARCHAR(100)
 );
 
 CREATE TABLE Utilisateur (
-    id INTEGER PRIMARY KEY,
-    role VARCHAR(50),
-    ville VARCHAR(100),
-    nom VARCHAR(100),
-    age INTEGER,
-    adresse VARCHAR(200),
-    telephone CHAR(11),
-    courriel VARCHAR(100),
-    prenom VARCHAR(100)
+  id        INTEGER AUTO_INCREMENT PRIMARY KEY,
+  role      VARCHAR(50),
+  ville     VARCHAR(100),
+  nom       VARCHAR(100),
+  age       INTEGER,
+  adresse   VARCHAR(200),
+  telephone CHAR(11),
+  courriel  VARCHAR(100),
+  prenom    VARCHAR(100)
 );
 
 CREATE TABLE Mot_de_passe (
-    id_utilisateur INTEGER PRIMARY KEY,
-    mot_de_passe BINARY(64)
+  id_utilisateur INTEGER AUTO_INCREMENT REFERENCES Utilisateur (id),
+  mot_de_passe   BINARY(64),
+  PRIMARY KEY (id_utilisateur)
 );
 
 CREATE TABLE Acheteur (
-    id_utilisateur INTEGER NOT NULL REFERENCES Utilisateur (id),
-    PRIMARY KEY (id_utilisateur)
+  id_utilisateur INTEGER NOT NULL AUTO_INCREMENT REFERENCES Utilisateur (id),
+  PRIMARY KEY (id_utilisateur)
 );
 
 CREATE TABLE Microbrasserie (
-    id_utilisateur INTEGER NOT NULL REFERENCES Utilisateur (id),
-    nom VARCHAR(100),
-    emplacement VARCHAR(200),
-    annee_inauguration INTEGER,
-    cote INTEGER,
-    PRIMARY KEY (id_utilisateur)
+  id_utilisateur     INTEGER NOT NULL REFERENCES Utilisateur (id),
+  nom                VARCHAR(100),
+  emplacement        VARCHAR(200),
+  annee_inauguration INTEGER,
+  cote               INTEGER,
+  PRIMARY KEY (id_utilisateur)
 );
 
 CREATE TABLE Biere (
-    id INTEGER PRIMARY KEY,
-    image_url VARCHAR(500),
-    prix FLOAT(2),
-    nom VARCHAR(100),
-    pourcentage_alcool FLOAT(1),
-    ibu INTEGER,
-    annee_production INTEGER,
-    mention VARCHAR(200),
-    id_microbrasserie INTEGER NOT NULL,
-    date_ajout DATE,
-    id_sorte INTEGER NOT NULL,
-    FOREIGN KEY (id_microbrasserie)
-        REFERENCES Microbrasserie (id_utilisateur),
-    FOREIGN KEY (id_sorte)
-        REFERENCES Sorte(id)
+  id                 INTEGER AUTO_INCREMENT PRIMARY KEY,
+  image_url          VARCHAR(500),
+  prix               FLOAT(2),
+  nom                VARCHAR(100),
+  pourcentage_alcool FLOAT(1),
+  ibu                INTEGER,
+  annee_production   INTEGER,
+  description        VARCHAR(200),
+  id_microbrasserie  INTEGER NOT NULL,
+  date_ajout         DATE,
+  id_sorte           INTEGER NOT NULL,
+  FOREIGN KEY (id_microbrasserie)
+  REFERENCES Microbrasserie (id_utilisateur),
+  FOREIGN KEY (id_sorte)
+  REFERENCES Sorte (id)
 );
 
 CREATE TABLE Achete (
-    id_acheteur INTEGER NOT NULL REFERENCES Acheteur (id_utilisateur),
-    id_biere INTEGER NOT NULL REFERENCES Biere (id),
-    quantite INTEGER NOT NULL,
-    date_achat TIMESTAMP,
-    PRIMARY KEY (id_acheteur , id_biere)
+  id_acheteur INTEGER NOT NULL AUTO_INCREMENT REFERENCES Acheteur (id_utilisateur),
+  id_biere    INTEGER NOT NULL REFERENCES Biere (id),
+  quantite    INTEGER NOT NULL,
+  date_achat  TIMESTAMP,
+  PRIMARY KEY (id_acheteur, id_biere)
 );
 
 CREATE TABLE Type_de (
-    id_sorte_parent INTEGER NOT NULL,
-    id_sorte_enfant INTEGER NOT NULL PRIMARY KEY REFERENCES Sorte (id),
-    FOREIGN KEY (id_sorte_parent)
-        REFERENCES Sorte (id)
+  id_sorte_parent INTEGER NOT NULL,
+  id_sorte_enfant INTEGER NOT NULL PRIMARY KEY REFERENCES Sorte (id),
+  FOREIGN KEY (id_sorte_parent)
+  REFERENCES Sorte (id)
 );
--- scripts/insertion_tests.sql
+++ scripts/insertion_tests.sql
@@ -1,17 +1,9 @@
-INSERT INTO Utilisateur(id, courriel, nom, prenom) VALUES(6, 'simon@test.ca', 'Lemay-Lauziere', 'Simon');
-INSERT INTO Utilisateur(id, courriel, nom, prenom) VALUES(7, 'keven@ulaval.ca', 'Lamontagne', 'Keven');
--- Simon : test
-INSERT INTO Mot_de_passe(id_utilisateur, mot_de_passe) VALUES(6, X'ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff');
--- Keven : ulaval
-INSERT INTO Mot_de_passe(id_utilisateur, mot_de_passe) VALUES(7, X'5c75dc48fdb6b2b3a8349899fd7963b9c043e7dffe720368ff2ef16dbd45afcb50869585671fa9d270ea411ee83b86dd8701f7a9eb986095e5f9a4305c8e2015');
-
-
 -- Création des représentants des microbrasseries
-INSERT INTO Utilisateur VALUES(1, 'Représentant Microbrasserie Dunham', 'Dunham', 'Tremblay', 30, '1111 rue de la Bière', '4183324323', 'Dunham@bière.ca', 'Jean');
-INSERT INTO Utilisateur VALUES(2, 'Representant Microbrasserie PitCaribou', 'Percé', 'Boivin', 32, '2222 rue de la Bière', '4183223534', 'PitCaribou@bière.ca', 'Marc');
-INSERT INTO Utilisateur VALUES(3, 'Representant Microbrasserie Vox Populi', 'Boucherville', 'Turcotte', 35, '3333 rue de la Bière', '4184345453', 'VoxPopuli@bière.ca', 'Étienne');
-INSERT INTO Utilisateur VALUES(4, 'Representant Microbrasserie Trou du Diable', 'Shawinigan', 'Chrétien', 40, '4444 rue de la Bière', '4184334324', 'TrouDuDiable@bière.ca', 'Jean');
-INSERT INTO Utilisateur VALUES(5, 'Representant Microbrasserie Auval', 'Gaspé', 'Lessard', 28, '5555 rue de la Bière', '4184334343', 'Auval@bière.ca', 'Guy');
+INSERT INTO Utilisateur(role, ville, nom, age, adresse, telephone, courriel, prenom) VALUES('Représentant Microbrasserie Dunham', 'Dunham', 'Tremblay', 30, '1111 rue de la Bière', '4183324323', 'Dunham@bière.ca', 'Jean');
+INSERT INTO Utilisateur(role, ville, nom, age, adresse, telephone, courriel, prenom) VALUES('Representant Microbrasserie PitCaribou', 'Percé', 'Boivin', 32, '2222 rue de la Bière', '4183223534', 'PitCaribou@bière.ca', 'Marc');
+INSERT INTO Utilisateur(role, ville, nom, age, adresse, telephone, courriel, prenom) VALUES('Representant Microbrasserie Vox Populi', 'Boucherville', 'Turcotte', 35, '3333 rue de la Bière', '4184345453', 'VoxPopuli@bière.ca', 'Étienne');
+INSERT INTO Utilisateur(role, ville, nom, age, adresse, telephone, courriel, prenom) VALUES('Representant Microbrasserie Trou du Diable', 'Shawinigan', 'Chrétien', 40, '4444 rue de la Bière', '4184334324', 'TrouDuDiable@bière.ca', 'Jean');
+INSERT INTO Utilisateur(role, ville, nom, age, adresse, telephone, courriel, prenom) VALUES('Representant Microbrasserie Auval', 'Gaspé', 'Lessard', 28, '5555 rue de la Bière', '4184334343', 'Auval@bière.ca', 'Guy');
 
 -- Création des microbrasseries
 INSERT INTO Microbrasserie VALUES(1, 'Brasserie Dunham', 'Dunham', 2011, 0);
@@ -19,6 +11,15 @@ INSERT INTO Microbrasserie VALUES(2, 'Pit Caribou', 'Anse-à-Beaufils', 2007, 0)
 INSERT INTO Microbrasserie VALUES(3, 'Microbrasserie Vox Populi', 'Boucherville', 2016, 0);
 INSERT INTO Microbrasserie VALUES(4, 'Trou du Diable', 'Shawinigan', 2006, 0);
 INSERT INTO Microbrasserie VALUES(5, 'Brasserie Auval', 'Val d\'Espoir', 2016, 0);
+
+-- Creation des utilisateurs de tests
+INSERT INTO Utilisateur(courriel, nom, prenom) VALUES('simon@test.ca', 'Lemay-Lauziere', 'Simon');
+INSERT INTO Utilisateur(courriel, nom, prenom) VALUES('keven@ulaval.ca', 'Lamontagne', 'Keven');
+-- Simon : test
+INSERT INTO Mot_de_passe(id_utilisateur, mot_de_passe) VALUES(1, X'ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff');
+-- Keven : ulaval
+INSERT INTO Mot_de_passe(id_utilisateur, mot_de_passe) VALUES(2, X'5c75dc48fdb6b2b3a8349899fd7963b9c043e7dffe720368ff2ef16dbd45afcb50869585671fa9d270ea411ee83b86dd8701f7a9eb986095e5f9a4305c8e2015');
+
 
 -- Création des sortes de bières selon CraftBeer.com
 
@@ -231,33 +232,33 @@ INSERT INTO Type_de VALUES(15, 94);
 INSERT INTO Type_de VALUES(15, 95);
 INSERT INTO Type_de VALUES(15, 96);
 
--- Ajout de bieres
-INSERT INTO Biere VALUES(1, '', 20.00,'Leo\'s Early Breakfast IPA', 6.2, 54, 2018, '', 1, '2018-04-06', 30);
-INSERT INTO Biere VALUES(2, '', 15.00, 'Supermoine Numéro 4', 4.5, NULL, 2018, '', 1, '2018-04-06', 46);
-INSERT INTO Biere VALUES(3, '', 15.00, 'Saison du Pinacle', 6.5, 52, 2018, '', 1, '2018-04-06', 16);
-INSERT INTO Biere VALUES(4, '', 15.00, 'Berliner Melon Weisse', 3.9, 8, 2018, '', 1, '2018-04-06', 37);
-INSERT INTO Biere VALUES(5, '', 15.00, 'LaBatt Porter Robuste', 6.0, 51, 2018, '', 1, '2018-04-06', 60);
+-- Ajout de Bieres
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 20.00,'Leo\'s Early Breakfast IPA', 6.2, 54, 2018, '', 1, '2018-04-06', 30);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 15.00, 'Supermoine Numéro 4', 4.5, NULL, 2018, '', 1, '2018-04-06', 46);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 15.00, 'Saison du Pinacle', 6.5, 52, 2018, '', 1, '2018-04-06', 16);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 15.00, 'Berliner Melon Weisse', 3.9, 8, 2018, '', 1, '2018-04-06', 37);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 15.00, 'LaBatt Porter Robuste', 6.0, 51, 2018, '', 1, '2018-04-06', 60);
 
-INSERT INTO Biere VALUES(6, '', 10.00, 'IPA Américaine des Appalaches', 7.0, 77, 2018, '', 2, '2018-04-06', 30);
-INSERT INTO Biere VALUES(7, '', 10.00, 'La Blonde de l\'Anse', 5.0, 17, 2018, '', 2, '2018-04-06', 46);
-INSERT INTO Biere VALUES(8, '', 10.00, 'Brown Ale Américaine', 7.0, 90, 2018, '', 2, '2018-04-06', 27);
-INSERT INTO Biere VALUES(9, '', 10.00, 'La Bonne Aventure', 5.0, 25, 2018, '', 2, '2018-04-06', 75);
-INSERT INTO Biere VALUES(10, '', 10.00, 'Gose IPA du Barachois', 3.8, 30, 2018, '', 2, '2018-04-06', 78);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 10.00, 'IPA Américaine des Appalaches', 7.0, 77, 2018, '', 2, '2018-04-06', 30);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 10.00, 'La Blonde de l\'Anse', 5.0, 17, 2018, '', 2, '2018-04-06', 46);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 10.00, 'Brown Ale Américaine', 7.0, 90, 2018, '', 2, '2018-04-06', 27);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 10.00, 'La Bonne Aventure', 5.0, 25, 2018, '', 2, '2018-04-06', 75);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 10.00, 'Gose IPA du Barachois', 3.8, 30, 2018, '', 2, '2018-04-06', 78);
 
-INSERT INTO Biere VALUES(11, '', 8.00, 'Double Fruit Punch IPA', 8.0, 100, 2018, '', 3, '2018-04-06', 32);
-INSERT INTO Biere VALUES(12, '', 8.00, 'Anna', 10.0, 75, 2018, '', 3, '2018-04-06', 50);
-INSERT INTO Biere VALUES(13, '', 8.00, 'Vox Stout(MilkShake)', 5.5, 25, 2018, '', 3, '2018-04-06', 65);
-INSERT INTO Biere VALUES(14, '', 8.00, 'Kettle Sour', 3.0, 3, 2018, '', 3, '2018-04-06', 37);
-INSERT INTO Biere VALUES(15, '', 8.00, 'Vox Pop IPA', 6.5, 45, 2018, '', 3, '2018-04-06', 30);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 8.00, 'Double Fruit Punch IPA', 8.0, 100, 2018, '', 3, '2018-04-06', 32);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 8.00, 'Anna', 10.0, 75, 2018, '', 3, '2018-04-06', 50);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 8.00, 'Vox Stout(MilkShake)', 5.5, 25, 2018, '', 3, '2018-04-06', 65);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 8.00, 'Kettle Sour', 3.0, 3, 2018, '', 3, '2018-04-06', 37);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 8.00, 'Vox Pop IPA', 6.5, 45, 2018, '', 3, '2018-04-06', 30);
 
-INSERT INTO Biere VALUES(16, '', 5.00, 'La Saison du Tracteur', 6.0, 35, 2018, '', 4, '2018-04-06', 16);
-INSERT INTO Biere VALUES(17, '', 5.00, 'Les Quatres Surfeurs de L\'Apocalypso', 6.5, 60, 2018, '', 4, '2018-04-06', 30);
-INSERT INTO Biere VALUES(18, '', 5.00, 'La Buteuse', 10.0, 60, 2018, '', 4, '2018-04-06', 50);
-INSERT INTO Biere VALUES(19, '', 5.00, 'Le Sang d\'Encre', 5.5, 45, 2018, '', 4, '2018-04-06', 66);
-INSERT INTO Biere VALUES(20, '', 5.00, 'La Morsure', 6.5, 77, 2018, '', 4, '2018-04-06', 30);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 5.00, 'La Saison du Tracteur', 6.0, 35, 2018, '', 4, '2018-04-06', 16);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 5.00, 'Les Quatres Surfeurs de L\'Apocalypso', 6.5, 60, 2018, '', 4, '2018-04-06', 30);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 5.00, 'La Buteuse', 10.0, 60, 2018, '', 4, '2018-04-06', 50);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 5.00, 'Le Sang d\'Encre', 5.5, 45, 2018, '', 4, '2018-04-06', 66);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 5.00, 'La Morsure', 6.5, 77, 2018, '', 4, '2018-04-06', 30);
 
-INSERT INTO Biere VALUES(21, '', 12.00, 'Super A', 8.0, 60, 2018, '', 5, '2018-04-06', 32);
-INSERT INTO Biere VALUES(22, '', 12.00, 'Saison Espinay', 6.5, 30, 2018, '', 5, '2018-04-06', 16);
-INSERT INTO Biere VALUES(23, '', 12.00, 'Arrière-Pays Grisette', 4.5, NULL, 2018, '', 5, '2018-04-06', 96);
-INSERT INTO Biere VALUES(24, '', 12.00, 'Arcane 17 IPA', 5.2, 60, 2018, '', 5, '2018-04-06', 30);
-INSERT INTO Biere VALUES(25, '', 12.00, 'Guerilla SSS', 7.8, 40, 2016, '', 5, '2018-04-06', 62);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 12.00, 'Super A', 8.0, 60, 2018, '', 5, '2018-04-06', 32);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 12.00, 'Saison Espinay', 6.5, 30, 2018, '', 5, '2018-04-06', 16);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 12.00, 'Arrière-Pays Grisette', 4.5, NULL, 2018, '', 5, '2018-04-06', 96);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 12.00, 'Arcane 17 IPA', 5.2, 60, 2018, '', 5, '2018-04-06', 30);
+INSERT INTO Biere(image_url, prix, nom, pourcentage_alcool, ibu, annee_production, description, id_microbrasserie, date_ajout, id_sorte) VALUES('', 12.00, 'Guerilla SSS', 7.8, 40, 2016, '', 5, '2018-04-06', 62);
