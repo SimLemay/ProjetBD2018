40a6de8f67072d37b952c779030e395162f12127
SimLemay <simon.lemay-lauziere.1@ulaval.ca>
2018-04-16 23:46:50 -0400

    Ajout du script de generation aleatoire

---
+++ script_insert.py
@@ -0,0 +1,25 @@
+from util import util_bd as bd
+
+LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam malesuada tristique ipsum, ac mollis " \
+              "felis viverra sit amet. Suspendisse consectetur sit amet massa vitae fringilla. Duis a purus a nisl " \
+              "rutrum finibus non vitae sem. Curabitur nisl dui, ultricies id bibendum quis, mattis sit amet posuere. "
+
+VILLES = ['Quebec', 'Saguenay']
+
+# Utilisateur
+requete = 'INSERT INTO Utilisateur (ville, nom, age, adresse, telephone, courriel, prenom) VALUES (%s, %s, %s, %s, %s, %s, %s);'
+for i in range(200):
+    print(i)
+
+# acheteur
+
+# Microbrasserie
+
+# Mot de passe
+
+# Biere
+
+# Vendre
+
+# Achete
+
